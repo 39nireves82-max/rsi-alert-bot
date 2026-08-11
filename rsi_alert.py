2fae6fd7923be06269a55e80e4f7ab8830adddd8
39nireves82-max <39nireves82@gmail.com>
2026-08-12 00:03:21 +0200

    Update rsi_alert.py

--- rsi_alert.py
+++ rsi_alert.py
@@ -42,10 +42,10 @@ def calc_rsi(df_ticker):
 
 # 3. Zu überwachende Ticker & individuelle Schwellenwerte
 WATCHLIST = [
-    {"sym": "PANW", "name": "Palo Alto Networks", "target_rsi": 99},
+    {"sym": "PANW", "name": "Palo Alto Networks", "target_rsi": 30},
     {"sym": "PLTR", "name": "Palantir Technologies", "target_rsi": 30},
     {"sym": "XYL", "name": "Xylem Inc.", "target_rsi": 35},
-    {"sym": "USPY", "name": "iShares S&P 500", "target_rsi": 35},
+    {"sym": "USPY", "name": "iShares S&P 500", "target_rsi": 99},
     {"sym": "IQQQ.DE", "name": "iShares Global Water", "target_rsi": 35},
     {"sym": "PG", "name": "Procter & Gamble", "target_rsi": 35},
     {"sym": "BTC-USD", "name": "Bitcoin USD", "target_rsi": 30},
